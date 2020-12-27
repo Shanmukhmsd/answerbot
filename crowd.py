68703cd7dfb57eb18bb2d8542b4c57d6ceb45898
Shanmukhmsd <76639888+Shanmukhmsd@users.noreply.github.com>
2020-12-27 10:59:25 +0530

    Update crowd.py

--- crowd.py
+++ crowd.py
@@ -244,8 +244,8 @@ class Bot(discord.Client):
 
         await self.clear_results()
         await self.update_embeds()
-        #await self.change_presence(activity=discord.Game(name='With I Am Best Bot Ever Made By ┻━┻◥ʙ ᴏ ꜱ ꜱ ➈™࿐ ...'))
-        await self.change_presence(activity=discord.Activity(type=1,name=' I Am Captain Cool'))
+        #await self.change_presence(activity=discord.Game(name='With I Am Best Bot Ever Made By shanmukh ©®⁷)
+        await self.change_presence(activity=discord.Activity(type=1,name='Mr.shanmukh'))
 
     async def on_message(self, message):
 
@@ -296,7 +296,7 @@ def bot_with_cyclic_update_process(update_event, answer_scores):
     upd_thread.start()
 
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start('Nzg4MDYxMDQ1ODA0NzYxMTE4.X9eBCg.ePcU1plkajIyy8_RbY0SHmgVT40'))
+    loop.create_task(bot.start('NzkyNjE3NjYwNjE2NTQwMTkw.X-gUuA.lqmn02dCinVVfMs3G-CYW4Sswho'))
     loop.run_forever()
 
 
@@ -305,7 +305,7 @@ def selfbot_process(update_event, answer_scores):
     selfbot = SelfBot(update_event, answer_scores)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(selfbot.start('NzAzODQ3Nzc5MjE0MDMyODk3.X-CJDA.h5GK4fZeROPFKaFZmjObLCTK-X8',
+    loop.create_task(selfbot.start('NzkyNjE3NjYwNjE2NTQwMTkw.X-gUuA.lqmn02dCinVVfMs3G-CYW4Sswho'))
                                    bot=False))
     loop.run_forever()
 
